37849f6d992db72b8e49e744918fedcebd6f30f4
Patrik200414 <martinpatrik14@gmail.com>
2024-07-03 11:51:02 +0200

    chor: initialize frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.js';
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
