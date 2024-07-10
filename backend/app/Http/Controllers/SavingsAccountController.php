fcf2078648d373d335ece573a3f7345420220d92
Patrik200414 <martinpatrik14@gmail.com>
2024-07-10 09:49:22 +0200

    refactor: handle integrity exception at createAccount method

--- backend/app/Http/Controllers/SavingsAccountController.php
+++ backend/app/Http/Controllers/SavingsAccountController.php
@@ -12,26 +12,33 @@ class SavingsAccountController extends AccountController
     use UserFromBearerToken;
 
     public function createAccount(AccountRequest $request){
-        $user = $this->getUserFromBearerToken($request);
+        try{
+            $user = $this->getUserFromBearerToken($request);
 
-        $account = new SavingAccount();
-        $account->user_id = $user->id;
-        $account->account_name= $request->accountName;
-        $account->balance = $request->balance;
-        $account->is_account_blocked = false;
-        $account->account_number = $request->accountNumber;
-        $account->monthly_interest = $request->monthlyInterest;
-        $account->monthly_maintenance_fee = $request->monthlyMaintenanceFee;
-        $account->transaction_fee = $request->transactionFee;
-        $account->last_interest_paied_at = $request->lastInterestPaiedAt;
-        $account->last_monthly_fee_paid_at = $request->lastMonthlyFeePaidAt; 
-        $account->minimum_balance = $request->minimumBalance;
-        $account->max_amount_of_transactions_monthly = $request->maxAmountOfTransactionsMonthly;
-        $account->last_avaible_transaction_date = $request->lastAvaibleTransactionDate;
-        $account->limit_exceeding_fee = $request->limitExceedingFee;
+            $account = new SavingAccount();
+            $account->user_id = $user->id;
+            $account->account_name= $request->accountName;
+            $account->balance = $request->balance;
+            $account->is_account_blocked = false;
+            $account->account_number = $request->accountNumber;
+            $account->monthly_interest = $request->monthlyInterest;
+            $account->monthly_maintenance_fee = $request->monthlyMaintenanceFee;
+            $account->transaction_fee = $request->transactionFee;
+            $account->last_interest_paied_at = $request->lastInterestPaiedAt;
+            $account->last_monthly_fee_paid_at = $request->lastMonthlyFeePaidAt; 
+            $account->minimum_balance = $request->minimumBalance;
+            $account->max_amount_of_transactions_monthly = $request->maxAmountOfTransactionsMonthly;
+            $account->last_avaible_transaction_date = $request->lastAvaibleTransactionDate;
+            $account->limit_exceeding_fee = $request->limitExceedingFee;
 
-        $account->save();
+            $account->save();
         return response()->json(['status'=>'Account successfully created!'], 202);
+        } catch(\PDOException $e){
+            $exceptionCode = $e->errorInfo[0];
+            if($exceptionCode === '23000'){
+                return response()->json(['error'=> 'An account with this account number is already exists!'], 409);
+            }
+        }
     }
     public function deleteAccount(string $accountId){
 
