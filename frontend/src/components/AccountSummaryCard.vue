08ad1c6dd9c7db3b85177c9f65e63bc0dc8dda49
Patrik200414 <martinpatrik14@gmail.com>
2024-07-13 14:34:23 +0200

    feat: implement show card summaries

---
+++ frontend/src/components/AccountSummaryCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+    accountSummary: Object
+});
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title"><span>Balance: </span>{{ props.accountSummary.balance }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ props.accountSummary.accountName }}</h6>
+            <h6 class="card-subtitle mb-2 text-muted">{{ props.accountSummary.accountNumber }}</h6>
+            <em>{{ props.accountSummary.accountType }}</em>
+            <br>
+            <div class="mt-3">
+                <a href="#" class="card-link btn btn-warning">Update</a>
+                <a href="#" class="card-link btn btn-danger">Delete</a>
+            </div>
+        </div>
+    </div>
+</template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,13 +1,50 @@
 <script setup>
-import router from '@/router';
+import DisplayErrorMessages from '@/components/DisplayErrorMessages.vue';
+import DisplayIsLoading from '@/components/DisplayIsLoading.vue';
+import AccountSummaryCard from '@/components/AccountSummaryCard.vue';
 import { getLogedInUser } from '@/utility/utility';
-import { onBeforeMount } from 'vue';
+import axios from 'axios';
+import { onBeforeMount, ref } from 'vue';
+
+const tokenName = import.meta.env.VITE_AUTH_KEY_NAME;
+
+const currUser = ref();
+const accountSummaries = ref([]);
+const errorMessage = ref();
+const isLoading = ref();
+
 
 onBeforeMount(() => {
-  getLogedInUser();
-})
+  currUser.value = getLogedInUser();
+  getAccountSummaries();
+});
+
+
+async function getAccountSummaries(){
+  try{
+    isLoading.value = true;
+    const accountSummariesResponse = await axios.get('/api/account/summary', {
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + currUser.value[tokenName]
+      }
+    });
+    accountSummaries.value = accountSummariesResponse.data.accounts;
+  } catch(error){ 
+    errorMessage.value = error.response.data.message;
+  } finally{
+    isLoading.value = false;
+  }
+}
 </script>
 
 <template>
-  <h1>Home page!</h1>
+  <div class="container d-flex flex-column justify-content-center align-items-center">
+    <h1 class="mt-5">Dash board</h1>
+    <div class="container d-flex flex-wrap justify-content-between mt-3">
+      <AccountSummaryCard class="m-2" v-for="accountSummary in accountSummaries" :key="accountSummary.id" :accountSummary="accountSummary"/>
+    </div>
+    <DisplayErrorMessages :errorMessages="[errorMessage]"/>
+    <DisplayIsLoading :isLoading="isLoading"/>
+  </div>
 </template>
